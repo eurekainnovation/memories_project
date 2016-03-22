3512e7706f78ff06758a888d78344ee9a5e6e428
robbiefryers <fryersr@tcd.ie>
2016-03-22 23:02:50 +0000

    nearly 0 errors on all pages

--- static/js/albums-ajax.js
+++ static/js/albums-ajax.js
@@ -32,7 +32,7 @@ function postmsg(form,val,photo,url) {
 
 //alert('something!! :D');
 if (!form.comment.value==""){
-document.getElementById(val+'new_msg').innerHTML +='<div style="backface-visibility: hidden;" class="row" id="thread"> <div style="backface-visibility: hidden;" class="col-xs-2"><img style="backface-visibility: hidden;" class="img-circle center-block" src='+ photo + ' alt="pic" height="50" width="50"></div><div style="backface-visibility: hidden;" class="col-xs-10 id=" message"=""><p  style="backface-visibility: hidden;">' + form.comment.value + '</p></div></div>';
+document.getElementById(val+'new_msg').innerHTML +='<div style="backface-visibility: hidden;" class="row thread"> <div style="backface-visibility: hidden;" class="col-xs-2"><img style="backface-visibility: hidden;" class="img-circle center-block" src='+ photo + ' alt="pic" height="50" width="50"></div><div style="backface-visibility: hidden;" class="col-xs-10 id=" message"=""><p  style="backface-visibility: hidden;">' + form.comment.value + '</p></div></div>';
 form.submit();
 form.comment.value = ""
 }
