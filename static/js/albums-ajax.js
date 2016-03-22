de7597981104f06a5d9326670771df1e6212b42e
robbiefryers <fryersr@tcd.ie>
2016-03-22 23:50:18 +0000

    very few errors left now

--- static/js/albums-ajax.js
+++ static/js/albums-ajax.js
@@ -32,9 +32,8 @@ function postmsg(form,val,photo,url) {
 
 //alert('something!! :D');
 if (!form.comment.value==""){
-document.getElementById(val+'new_msg').innerHTML +='<div style="backface-visibility: hidden;" class="row thread"> <div style="backface-visibility: hidden;" class="col-xs-2"><img style="backface-visibility: hidden;" class="img-circle center-block" src='+ photo + ' alt="pic" height="50" width="50"></div><div style="backface-visibility: hidden;" class="col-xs-10 id=" message"=""><p  style="backface-visibility: hidden;">' + form.comment.value + '</p></div></div>';
+document.getElementById(val+'new_msg').innerHTML +='<div style="backface-visibility: hidden;" class="row thread"> <div style="backface-visibility: hidden;" class="col-xs-2"><img style="backface-visibility: hidden;" class="img-circle center-block" src='+ photo + ' alt="pic" height="50" width="50"></div><div style="backface-visibility: hidden;" class="col-xs-10 message"><p  style="backface-visibility: hidden;">' + form.comment.value + '</p></div></div>';
 form.submit();
-form.comment.value = ""
 }
 
 
